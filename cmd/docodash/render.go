6bda3dfcbe41f2d995e7696c2df326621871a86d
Henrik Jernevad <henrik@jernevad.se>
2024-09-24 21:02:20 +0200

    Remove dead/unnecessary code.

--- cmd/docodash/render.go
+++ cmd/docodash/render.go
@@ -4,7 +4,6 @@ import (
 	"html/template"
 	"net/http"
 	"path/filepath"
-	"reflect"
 )
 
 func RenderDocument(w http.ResponseWriter, err error, project Project) {
@@ -23,35 +22,4 @@ func RenderDocument(w http.ResponseWriter, err error, project Project) {
 	if err != nil {
 		InternalServerError(w, err, "Unable to render template", "")
 	}
-}
-
-// isArrayOrSlice checks if the passed value is an array or slice.
-func isArrayOrSlice(v interface{}) bool {
-	rv := reflect.ValueOf(v)
-	return rv.Kind() == reflect.Array || rv.Kind() == reflect.Slice
-}
-
-// isMap checks if the passed value is a map.
-func isMap(v interface{}) bool {
-	return reflect.ValueOf(v).Kind() == reflect.Map
-}
-
-// isStruct checks if the passed value is a struct.
-func isStruct(v interface{}) bool {
-	return reflect.ValueOf(v).Kind() == reflect.Struct
-}
-
-// structFields returns a map of field names and values for a struct.
-func structFields(v interface{}) map[string]interface{} {
-	rv := reflect.ValueOf(v)
-	rt := rv.Type()
-	fields := make(map[string]interface{})
-
-	// Loop over struct fields and add them to the map
-	for i := 0; i < rv.NumField(); i++ {
-		fieldName := rt.Field(i).Name
-		fieldValue := rv.Field(i).Interface()
-		fields[fieldName] = fieldValue
-	}
-	return fields
 }
